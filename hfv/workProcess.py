8cadeb50cfb1fe18e74e063670357457675150cb
jio jio <joliu@s-an.org>
2018-04-09 12:25:47 +0000

    开发输入与输出分离的任务执行逻辑

--- hfv/workProcess.py
+++ hfv/workProcess.py
@@ -19,9 +19,39 @@ from listenSer import sendCommandToDevice
 import sqlite3
 import time
 
-def findTask():
+
+def findInputTask():
+    # 从输入任务队列中获取任务
+    inputTaskList = []
+    (status, output) = findTask("inputtask")
+    if status == -1:
+        return (status, output)
+    # condition:条件值， taskId：任务id， waitTime:任务执行等待时间
+    for task in output:
+        inputTaskList.append(task)
+    return inputTaskList
+    
+
+def findOutputTask(taskId):
+    # 通过任务id查找任务
+    outputTaskList = []
+    (status, output) = findTask("outputtask", taskId)
+    if status == -1:
+        return (status, output)
+    # taskId: 任务id，nodeAddr:输出地址，countNum: 计数器
+    for task in output:
+        outputTaskList.append(task)
+    return outputTaskList
+
+     
+def findTask(task_name, taskId=0):
     try:
-        sql = "select * from task"
+        conn = sqlite3.connect("task.db")
+        cursor = conn.cursor()
+        if taskId == 0:
+            sql = "select * from " + task_name
+        else:
+            sql = "select * from " + task_name + " where id='" + taskId + "'"
         conn = sqlite3.connect("task.db")
         cursor = conn.cursor()
         cursor.execute(sql)
@@ -37,7 +67,28 @@ def findTask():
     finally:
         return (status, output)
 
+def makeTask(inputTaskList):
+    # 构建任务指令
+    taskList = []
+    for inputTask in inputTaskList:
+        (condition, taskId, waitTime) = inputTask
+        outputTaskList = findOutputTask(taskId)
+        for outputTask in outputTaskList:
+            # 获取输出节点信息
+            (taskId, dstAddrPortCmd, countNum) = outputTask
+            (addr,port, cmd) = dstAddrCmd.split(':')
+            # 读取传感器数据
+            (status, output) = sendCommandToDevice(method)
+            # 对dht11做特殊处理，以后需改nodeMCU代码建议改善
+            data = data.split('&')[0]
+            print("get data from sensor " + output)
+            if status == -1:
+                print("get device data failed! ip: %s, method: %s" % (ip, method))
+                return (status, "get device data failed! ip: %s, method: %s" % (ip, method))
 
+            if compare(condition[0], float(data), flaoat(contdition[1:])):
+        
+  
 # 根据任务队列中任务，依次执行
 def doTask():
     (status, outputs) = findTask()
@@ -96,4 +147,6 @@ def mainWhileProcess(input_ctime):
 
 
 if __name__ == "__main__":
-    mainWhileProcess(5)
+    #mainWhileProcess(5)
+    print(findInputTask())
+    print(findOutputTask("58278258"))
